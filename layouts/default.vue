61c76194e9278ac28ce268d549529d94990ff3be
Daniele Lenares <daniele.lenares@gmail.com>
2019-03-10 11:56:31 +0100

    Initial commit.

---
+++ layouts/default.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <nav
+      class="navbar header has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation">
+      <div class="navbar-brand">
+        <a
+          class="navbar-item logo"
+          href="/">
+          dnlrns
+        </a>
+        <div class="navbar-burger">
+          <span/>
+          <span/>
+          <span/>
+        </div>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item">
+            Who am I
+          </div>
+          <div class="navbar-item">
+            Résumé
+          </div>
+          <div class="navbar-item">
+            Stuff I find online
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="main-content columns">
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.logo
+  font-family 'Fonarto', sans-serif
+  font-size 23px
+</style>
+
+
