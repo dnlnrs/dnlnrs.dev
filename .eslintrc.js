a61d818ce51b06c72c92ed835d6aead9d1ec5b6b
Daniele Lenares <dnlnrs@MBP-di-Daniele.lan>
2019-05-25 10:41:41 +0200

    Fixes eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,8 +19,10 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/singleline-html-element-content-newline': 'off',
-    'vue/max-attributes-per-line': 'off',
+    'vue/max-attributes-per-line': ['warn', { 'singleline': 4 }],
     'vue/html-self-closing': 'off',
-    'vue/attributes-order': 'off'
+    'vue/attributes-order': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/html-closing-bracket-spacing': 'off'
   }
 }
