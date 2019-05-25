52f96e42d664be06a99ff21495cd1f44e67b695b
Daniele Lenares <daniele.lenares@gmail.com>
2019-05-25 19:03:27 +0200

    Fixes viewport metatag.

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
     title: 'Daniele Lenares | Full Stack Web Developer',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
