6349bd82d84c33d149bb9da31ec2bd512eedfa1e
Daniele Lenares <daniele.lenares@gmail.com>
2019-07-08 12:59:45 +0200

    Changes docker config.

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,10 @@ const pkg = require('./package')
 module.exports = {
   mode: 'universal',
 
+  server: {
+    host: process.env.HOST || '127.0.0.1',
+    port: process.env.PORT || 3000
+  },
   /*
   ** Headers of the page
   */
