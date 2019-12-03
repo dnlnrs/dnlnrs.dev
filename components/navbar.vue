ca9fafdf63a8a71f6c1f549c3a26c676be9d3fb6
Daniele Lenares <daniele.lenares@gmail.com>
2019-12-03 15:01:22 +0100

    chore: some edits

---
+++ components/navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav
+    class="navbar header is-fixed-top"
+    role="navigation"
+    aria-label="main navigation">
+    <div class="navbar-brand">
+      <nuxt-link class="navbar-item logo" to="/" @click.native="mobileMenuActive = false">
+        dnlnrs
+      </nuxt-link>
+      <div :class="`navbar-burger ${mobileMenuActive ? 'is-active' : ''}`" @click="toggleMobileMenu">
+        <span/>
+        <span/>
+        <span/>
+      </div>
+    </div>
+    <div :class="`navbar-menu ${mobileMenuActive ? 'is-active' : ''}`">
+      <div class="navbar-end">
+        <nuxt-link class="navbar-item" to="/who-am-i" @click.native="mobileMenuActive = false">
+          <span class="icon">
+            <i class="las la-user"></i>
+          </span>
+          <span>Who am I</span>
+        </nuxt-link>
+        <a class="navbar-item" href="https://drive.google.com/file/d/19KGcsqEoaJAqgnSmGSzksLA4xhSLTrZ_/view?usp=sharing" target="_blank">
+          <span class="icon">
+            <i class="las la-file-pdf"></i>
+          </span>
+          <span>Résumé</span>
+        </a>
+        <div class="navbar-item inactive">
+          Stuff I found online
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    mobileMenuActive: false
+  }),
+  methods: {
+    toggleMobileMenu () {
+      this.mobileMenuActive = !this.mobileMenuActive
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.logo
+  font-family 'Fonarto', sans-serif
+  font-size 25px
+.inactive
+  opacity .5
+.nuxt-link-active
+  color: #008080;
+.navbar-item
+  i
+    font-size: 1.2rem
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,49 +1,6 @@
 <template>
   <div>
-    <nav
-      class="navbar header is-fixed-top"
-      role="navigation"
-      aria-label="main navigation">
-      <div class="navbar-brand">
-        <nuxt-link class="navbar-item logo" to="/" @click.native="mobileMenuActive = false">
-          dnlrns
-        </nuxt-link>
-        <div :class="`navbar-burger ${mobileMenuActive ? 'is-active' : ''}`" @click="toggleMobileMenu">
-          <span/>
-          <span/>
-          <span/>
-        </div>
-      </div>
-      <div :class="`navbar-menu ${mobileMenuActive ? 'is-active' : ''}`">
-        <div class="navbar-end">
-          <nuxt-link class="navbar-item" to="/who-am-i" @click.native="mobileMenuActive = false">
-            Who am I
-          </nuxt-link>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              Résumé
-            </a>
-            <div class="navbar-dropdown is-boxed">
-              <a target="_blank" href="https://drive.google.com/open?id=0B3RKPTAd6l8OVWpaYW5KdjlmbVE" class="navbar-item">
-                <span class="icon">
-                  <i class="fas fa-file-pdf"></i>
-                </span>
-                <p>Italian</p>
-              </a>
-              <a target="_blank" class="navbar-item inactive">
-                <span class="icon">
-                  <i class="fas fa-file-pdf"></i>
-                </span>
-                <p>English</p>
-              </a>
-            </div>
-          </div>  
-          <div class="navbar-item inactive">
-            Stuff I found online
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Navbar />
     <section class="main-content">
       <nuxt />
     </section>
@@ -51,35 +8,13 @@
 </template>
 
 <script>
+import Navbar from '~/components/navbar'
+
 export default {
-  head() {
-    return {
-      htmlAttrs: {
-        class: 'has-navbar-fixed-top'
-      }
-    }
-  },
-  data () {
-    return {
-      mobileMenuActive: false
-    }
-  },
-  methods: {
-    toggleMobileMenu () {
-      this.mobileMenuActive = !this.mobileMenuActive
-    }
+  components: {
+    Navbar
   }
 }
 </script>
-
-<style lang="stylus" scoped>
-.logo
-  font-family 'Fonarto', sans-serif
-  font-size 25px
-.inactive
-  opacity .5
-.nuxt-link-active
-  color: #008080;
-</style>
 
 
