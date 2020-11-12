8a4fea69c98e4cbe95a3d7c0154aae18c0c96017
Daniele Lenares <daniele.lenares@gmail.com>
2020-11-12 07:59:56 +0100

    feat: add snippets and blog pages placeholders

---
+++ pages/snippets.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="columns is-vcentered">
+    <div class="column is-6 has-text-centered">
+      Coming soon! :)
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
